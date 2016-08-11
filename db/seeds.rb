3a16ccbc5b5c9f49d47241c82c6122c6c3cdfb82
frankdew <frankdew23@gmail.com>
2016-08-11 13:35:27 +0200

    Add Show post user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,24 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Post.delete_all
-
-Post.create( content: "Pizza is yummy!"                    )
-Post.create( content: "Veggies are tasty and nutritious."  )
-Post.create( content: "Salt is the best ingredient."       )
-
-Post.create( content: "I've seen Star Wars 23 times."      )
-Post.create( content: "Walking Dead has an exciting plot." )
-Post.create( content: "I only watch art-house films."      )
-
-Post.create( content: "My cats are cute!"      )
-Post.create( content: "Dogs are good friends."  )
-Post.create( content: "My hamster resents me." )
-
-Post.create( content: "Free market solves all problems."         )
-Post.create( content: "Workers control the means of production." )
-Post.create( content: "Can't we all just get along?"             )
-
 User.delete_all
 
 frank = User.create( email: 'frank@ex.com', password: 'abcd1234' )
@@ -46,3 +28,21 @@ Post.create( content: "My hamster resents me." , user: anton )
 Post.create( content: "Free market solves all problems."         , user: frank )
 Post.create( content: "Workers control the means of production." , user: alice )
 Post.create( content: "Can't we all just get along?"             , user: anton )
+
+Post.delete_all
+
+Post.create( content: "Pizza is yummy!"                    )
+Post.create( content: "Veggies are tasty and nutritious."  )
+Post.create( content: "Salt is the best ingredient."       )
+
+Post.create( content: "I've seen Star Wars 23 times."      )
+Post.create( content: "Walking Dead has an exciting plot." )
+Post.create( content: "I only watch art-house films."      )
+
+Post.create( content: "My cats are cute!"      )
+Post.create( content: "Dogs are good friends."  )
+Post.create( content: "My hamster resents me." )
+
+Post.create( content: "Free market solves all problems."         )
+Post.create( content: "Workers control the means of production." )
+Post.create( content: "Can't we all just get along?"             )
