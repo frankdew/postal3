ad7b46488cd4aa4d1df556679c2616bc12b2abc9
frankdew <frankdew23@gmail.com>
2016-08-11 14:15:33 +0200

    User's posts page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,4 +16,10 @@ class PostsController < ApplicationController
       render posts_path
     end
   end
+
+  def user
+      @user = User.find( params[:user_id] )
+
+      @posts = Post.where( user: @user ).order( created_at: :desc )
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   resources :posts
   root 'posts#index'
+  get 'user_posts/:user_id' => 'posts#user', as: :user_posts
 end
